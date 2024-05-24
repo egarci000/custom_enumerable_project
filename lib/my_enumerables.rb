df618efa12d17334584a98dd9f176066774aed3c
egarci000 <egarci9000@gmail.com>
2024-05-23 21:13:42 -0500

    Complete custom enumerable project

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -8,4 +8,81 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    return self if yield(self[0]) == nil
+    i = 1
+    while i < self.length
+      yield(self[i])
+      i += 1
+    end
+  end
+
+  def my_each_with_index
+    i = 0
+    self.each do |elem|
+      yield(elem, i)
+      i += 1
+    end
+  end
+
+  def my_select
+    arr = []
+    self.each do |elem|
+      arr << elem if yield(elem)
+    end
+    arr
+  end
+
+  def my_all?
+    arr = []
+    self.each do |elem|
+      arr << yield(elem)
+    end
+
+    arr.each do |elem|
+      return false if elem == false
+    end
+    true
+  end
+
+  def my_any?
+    self.each do |elem|
+      return true if yield(elem)
+    end
+    false
+  end
+
+  def my_none?
+    self.each do |elem|
+      return false if yield(elem)
+    end
+    true
+  end
+
+  def my_count
+    return self.length unless block_given?
+    arr = []
+    self.each do |elem|
+      arr << elem if yield(elem)
+    end
+    arr.length
+  end
+
+  def my_map
+    arr = []
+    self.each do |elem|
+      arr << yield(elem)
+    end
+    arr
+  end
+
+  def my_inject(initial_value)
+    sum = initial_value
+    arr = []
+    self.each do |elem|
+      sum = yield(sum, elem)
+      arr << sum
+    end
+    arr[-1]
+  end
 end
